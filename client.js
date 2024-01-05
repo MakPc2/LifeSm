40240900b8f458b8fca082f37008ce3cf0454e2f
Mak <153941612+MakPc2@users.noreply.github.com>
2024-01-06 02:16:30 +0700

    Add files via upload

--- client.js
+++ client.js
@@ -4,11 +4,12 @@ ban_area.Tags = ["Ban"];
 ban_area.OnEnter.Add(function(dp, a) {
     try {
         let p = Players.GetByRoomId(a.Name);
-        p.SetPositionAndRotation(0, 0);
+        p.SetPositionAndRotation({ x: NaN y: NaN, z: NaN }, { x: 1, y: 1 });
         p.Damage.DamageIn.Value = false;
         p.Ui.Hint.Value = "Вы были забанены!";
     } catch (err) {
         dp.Ui.Hint.Value = "Игрок не найден.";
+        dp.PopUp(err.name + "\n" + err.message);
     }
     
     a.Tags.Clear();
@@ -22,6 +23,7 @@ b_t.Spawns.SpawnPointsGroups.Add(1);
 Teams.OnRequestJoinTeam.Add(function(p,t) { t.add(p); });
 Teams.OnPlayerChangeTeam.Add(function(p) {
     p.Spawns.Spawn(); 
+    p.Properties.Get("rd").Value = p.IdInRoom;
     if (p.IdInRoom == 1) {
         p.Build.BuildRangeEnable.Value = true;
     }
