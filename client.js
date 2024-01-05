64e30d85a32d773d68d280dce0193a081635e062
Mak <153941612+MakPc2@users.noreply.github.com>
2024-01-06 02:19:18 +0700

    Add files via upload

--- client.js
+++ client.js
@@ -4,7 +4,7 @@ ban_area.Tags = ["Ban"];
 ban_area.OnEnter.Add(function(dp, a) {
     try {
         let p = Players.GetByRoomId(a.Name);
-        p.SetPositionAndRotation({ x: NaN y: NaN, z: NaN }, { x: 1, y: 1 });
+        p.SetPositionAndRotation({ x: NaN, y: NaN, z: NaN }, { x: 1, y: 1 });
         p.Damage.DamageIn.Value = false;
         p.Ui.Hint.Value = "Вы были забанены!";
     } catch (err) {
