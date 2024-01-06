142e513ff3ef8c8ef4e52ae0bb835350efe9ed8a
Mak <153941612+MakPc2@users.noreply.github.com>
2024-01-07 00:56:53 +0700

    Add files via upload

--- client.js
+++ client.js
@@ -1,51 +1,89 @@
-BreackGraph.Damage = false;
+// Константы
+const SAVE = [
+    [ "IsDeath", false ], [ "Hp", 100 ]
+],
+PROPS = Properties.GetContext(),
+MSGS_LOADER = ["<b>Загрузка...</b>", "Читайте инструкцию!", "Что ты тут забыл?"]
 
-var ban_area = AreaPlayerTriggerService.Get("Ban");
-ban_area.Enable = true;
-ban_area.Tags = ["Ban"];
-ban_area.OnEnter.Add(function(dp, a) {
-    try {
-        let p = Players.GetByRoomId(a.Name);
-        p.SetPositionAndRotation({ x: NaN, y: NaN, z: NaN }, { x: 1, y: 1 });
-        p.Damage.DamageIn.Value = false;
-        p.Ui.Hint.Value = "Вы были забанены!";
-        
-        let e = Players.GetEnumerator();
-        while (e.MoveNext()) {
-            if (e.Current.Id != p.Id) e.Current.Spawns.Spawn();
+// Созданик команд
+Teams.Add("Blue", "Blue", { b: 1 });
+var blueTeam = Teams.Get("Blue");
+blueTeam.Build.BlocksSet.Value = BuildBlocksSet.Blue;
+blueTeam.Spawns.SpawnPointsGroups.Add(1);
+
+// Выключаем инвентарь
+["Main", "Secondary", "Build", "Explosive"].forEach(function(weapon) {
+    Inventory.GetContext()[weapon].Value = false;
+});
+
+// Вход в команду при заходе на сервер
+Teams.OnRequestJoinTeam.Add(function(p, t) { t.add(p); });
+
+Players.OnPlayerConnected.Add(function(p) {
+    outp(p);
+    
+    let prop = p.Properties;
+    if (prop.Get("IsDeath").Value) return ban(p);
+    
+    // Спец - значения
+    p.contextedProperties.MaxHp.Value = prop.Get("Hp").Value;
+    
+    p.Timers.Get("Respawn").RestartLoop(1);
+});
+
+Teams.OnPlayerChangeTeam.Add(function(p) { 
+    if (p.Properties.Get("IsDeath").Value) return;
+    p.Spawns.Spawn(); 
+});
+
+// Слхраняемся
+Players.OnPlayerDisconnected.Add(function(p) {
+    save(p);
+    
+    // Спец значения
+    let prop = p.Properties;
+});
+
+// Баним игрока при смерти
+Damage.OnDeath.Add(function(p) { Ban(p); });
+
+function ban(p) {
+    p.Properties.Get("IsDeath").Value = true;
+    p.Spawns.Spawn();
+    p.Spawns.Despawn();
+}
+
+// Счетчик Hp
+Damage.OnDamage.Add(function(p, p2, dmg) {
+    p.Properties.Get("Hp").Value -= Math.ceil(dmg);
+});
+
+// Таймеры
+Timers.OnPlayerTimer.Add(function(t) {
+    let p = t.Player, id = t.Id, prop = p.Properties;
+    
+    if (id === "Respawn") {
+        prop.Get("Respawn-indx").Value += 1;
+        if (prop.GetContext("Respawn-indx").Value > 10) {
+            blueTeam.add(p);
+            return t.Stop();
         }
         
-    } catch (err) {
-        dp.Ui.Hint.Value = "Игрок не найден."
-        dp.PopUp(err.name + "\n" + err.message);
-    }
-    
-    a.Tags.Clear();
-    a.Ranges.Clear();
-});
-
-Teams.Add("Blue","Blue",{ b: 1 });
-var b_t = Teams.Get("Blue");
-b_t.Spawns.SpawnPointsGroups.Add(1);
-
-Teams.OnRequestJoinTeam.Add(function(p,t) { 
-    t.add(p); 
-    if (p.IdInRoom == 1) { Properties.GetContext().Get("Id").Value = p.Id; }
-    if (p.Id == Properties.GetContext().Get("Id").Value) {
-        p.Build.BuildRangeEnable.Value = true;
-        p.Build.FlyEnable.Value = true;
+        p.Ui.Hint.Value = ("<b>Загрузка...</b>"
+            + MSGS_LOADER[Math.floor(Math.random() * MSGS_LOADER.length)]);
     }
 });
-Teams.OnPlayerChangeTeam.Add(function(p) {
-    p.Spawns.Spawn(); 
-    p.Properties.Get("rd").Value = p.IdInRoom;
-});
 
-LeaderBoard.PlayerLeaderBoardValues =
-[
-    {
-        Value: "rd",
-        DisplayName: "rd",
-        ShortDisplayName: "rd"
-    }
-]
+// Сохранение данных на сервер
+function save(p) {
+    SAVE.forEach(function(el) {
+        PROPS.Get(el[0] + p.Id).Value = p.Properties.Get(el[0]).Value;
+    });
+}
+
+// Вывод данных в игрока
+function outp(p) {
+    SAVE.forEach(function(el) {
+        p.Properties.Get(el[0]).Value = PROPS.Get(el[0] + p.Id).Value || el[1];
+    });
+}
