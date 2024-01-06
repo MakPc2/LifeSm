a096e08d64d43ef027ca48f7de5d29384fc8de2d
Mak <153941612+MakPc2@users.noreply.github.com>
2024-01-07 03:04:58 +0700

    Add files via upload

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 // Константы
 const SAVE = [
-    [ "IsDeath", false ], [ "Hp", 100 ]
-],
+    [ "IsDeath", false ], [ "Hp", 100 ], [ "Kills", 0 ]
+], 
 PROPS = Properties.GetContext(),
 MSGS_LOADER = ["<b>Загрузка...</b>", "Читайте инструкцию!", "Что ты тут забыл?", "Кто то вообще это читает?...", "Карл?!"];
 
@@ -12,9 +12,7 @@ blueTeam.Build.BlocksSet.Value = BuildBlocksSet.Blue;
 blueTeam.Spawns.SpawnPointsGroups.Add(1);
 
 // Выключаем инвентарь
-["Main", "Secondary", "Build", "Explosive"].forEach(function(weapon) {
-    Inventory.GetContext()[weapon].Value = false;
-});
+new inventory(["Main", "Secondary", "Build", "Explosive"], false, room);
 
 // Вход в команду при заходе на сервер
 Teams.OnRequestJoinTeam.Add(function(p, t) {
@@ -43,12 +41,7 @@ Teams.OnPlayerChangeTeam.Add(function(p) {
 });
 
 // Слхраняемся
-Players.OnPlayerDisconnected.Add(function(p) {
-    save(p);
-    
-    // Спец значения
-    let prop = p.Properties;
-});
+Players.OnPlayerDisconnected.Add(function(p) { save(p); });
 
 // Баним игрока при смерти
 Damage.OnDeath.Add(function(p) { ban(p); });
@@ -61,25 +54,42 @@ function ban(p) {
 }
 
 // Счетчик Hp
-Damage.OnDamage.Add(function(p, p2, dmg) {
-    p.Properties.Get("Hp").Value -= Math.ceil(dmg);
-});
+Damage.OnDamage.Add(function(p, p2, dmg) { p.Properties.Get("Hp").Value -= Math.ceil(dmg); });
 
 // Таймеры
 Timers.OnPlayerTimer.Add(function(t) {
     let p = t.Player, id = t.Id, prop = p.Properties;
     
-    if (id == "Respawn") {
-        prop.Get("Respawn-indx").Value += 1;
-        
-        if (prop.Get("Respawn-indx").Value > 10) {
-            blueTeam.add(p);
-            return t.Stop();
-        }
-        
-        p.Ui.Hint.Value = ("<b>Загрузка...</b>" + "\n" +
-            + MSGS_LOADER[Math.floor(Math.random() * MSGS_LOADER.length)]);
+    switch (id) {
+        case "Respawn":
+            prop.Get("Respawn-indx").Value += 1;
+            
+            if (prop.Get("Respawn-indx").Value > 10) {
+                blueTeam.add(p);
+                return t.Stop();
+            }
+            
+            p.Ui.Hint.Value = ("<b>Загрузка...</b>" + "\n" +
+                + MSGS_LOADER[Math.floor(Math.random() * MSGS_LOADER.length)]);
+            break;
+        case "Reset": p.Ui.Hint.Reset(); break;
     }
+});
+
+// Зоны
+new Trigger("NoBuild", ["NoBuild"], true, function(p) {
+    p.inventory.Build.Value = false;
+    p.Ui.Hint.Value = "строительство запрещено";
+    p.Timers.Get("Reset").Restart(5);
+},                                        function(p) {
+    p.inventory.Build.Value = true;
+});
+
+let invs = ["Main", "Secondary", "Explosive"];
+new Trigger("NoWeapon", ["NoWeapon"], true, function(p) {
+    new inventory(invs, false, p);
+},                                          function(p) {
+    new inventory(invs, true,  p);
 });
 
 // Сохранение данных на сервер
@@ -93,5 +103,22 @@ function save(p) {
 function outp(p) {
     SAVE.forEach(function(el) {
         p.Properties.Get(el[0]).Value = PROPS.Get(el[0] + p.Id).Value || el[1];
+    });
+}
+
+// Создание триггера и виазулизатора
+function Trigger(Name, Tags, Enable, Exit, Enter) {
+    let trigger = AreaPlayerTriggerService.Get(Name);
+    trigger.Enable = Enable;
+    trigger.Tags = Tags;
+    trigger.OnEnter.Add(Enter);
+    trigger.OnExit.Add(Exit);
+    return trigger;
+}
+
+// Сброс инвентаря
+function inventory(Wps, _Value, Ctx) {
+    Wps.forEach(function(wp) {
+        Inventory.GetContext(Ctx)[wp].Value = _Value;
     });
 }
